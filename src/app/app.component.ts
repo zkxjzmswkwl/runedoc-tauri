ffe9d58ed5fed335265d8c5a12467d20452fa3b5
Dylan Warren <dylanwarren19@gmail.com>
2024-11-04 14:36:34 -0600

    update: configure services/feature states and show example state save/load in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,31 @@ import { Lara } from 'primeng/themes/lara';
 import { HeaderComponent } from './header/header.component';
 import { AlertComponent } from './shared/components/alert/alert.component';
 import { AccordionComponent } from './shared/components/accordion/accordion.component';
+import { Store } from 'tauri-plugin-store-api';
+import { combineLatest, from, Observable } from 'rxjs';
+import { GlobalService } from './shared/services/global.service';
+import { GlobalFeatureState } from './shared/state/global.feature';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+/**
+ * @TODO Use later on if there are settings that aren't tied to a users RS account
+ * but are RuneDoc related that a user may want to see saved.
+ * @param file {@link AppComponent.settingsLocation} Where the Tauri file saves at in AppData
+ * @param key Object key to save as.
+ * @param data The data we wish to save.
+ */
+async function saveToStore<T>(file: string, key: string, data: T) {
+  const store = new Store(file);
+
+  await store.set(key, { value: data });
+  await store.save();
+}
+
+function getFromStore<T>(file: string, key: string): Observable<{ value: T } | null> {
+  const store = new Store(file);
+
+  return from(store.get<{ value: T }>(key));
+}
 
 @Component({
   selector: 'app-root',
@@ -19,11 +44,26 @@ import { AccordionComponent } from './shared/components/accordion/accordion.comp
 })
 export class AppComponent {
   private readonly pngConfig = inject(PrimeNGConfig);
+  private readonly globalService = inject(GlobalService);
+  private readonly settingsLocation = '.settings.json';
 
   greetingMessage = '';
 
   constructor() {
     this.pngConfig.theme.set({ preset: Lara });
+
+    /**
+     * combineLatest for future proofing multiple states.
+     */
+    combineLatest([
+      getFromStore<GlobalFeatureState>(this.settingsLocation, 'global_settings'),
+    ])
+      .pipe(takeUntilDestroyed())
+      .subscribe(([globalState]) => {
+        // We currently don't save the data, this is more of an example of how data should be loaded
+        // when there are more services saving.
+        this.globalService.updateState(globalState?.value ?? {});
+      });
   }
 
   greet(event: SubmitEvent, name: string): void {
