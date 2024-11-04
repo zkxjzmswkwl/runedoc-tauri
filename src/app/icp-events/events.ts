994289281a44cfd38639aa59181019d465105dcc
Dylan Warren <dylanwarren19@gmail.com>
2024-11-04 14:36:05 -0600

    update: moved packet listeners to their respective services

--- src/app/icp-events/events.ts
+++ src/app/icp-events/events.ts
@@ -1,12 +1,4 @@
-import { listen } from '@tauri-apps/api/event';
 import { invoke } from '@tauri-apps/api/core';
-import { handleResponseRSN } from './packethandlers/rsn';
-import { handleResponseMetrics } from './packethandlers/metrics';
-
-const incomingHandlers: Map<string, (buffer: string) => void> = new Map([
-  ['rsn', (buffer: string) => handleResponseRSN(buffer)],
-  ['metric', (buffer: string) => handleResponseMetrics(buffer)],
-]);
 
 export function setupListeners() {
   invoke('start_tcp_listener');
@@ -15,25 +7,3 @@ export function setupListeners() {
 export function queuePacket(packetType: string, packetIdentifier: string, ...args: string[]) {
   invoke('queue_outgoing_packet', { packet: `${packetType}:${packetIdentifier}:${args.join(':')}<dongs>` });
 }
-
-listen<string>('packet_received', (event) => {
-  const buffer = event.payload;
-
-  if (!buffer.startsWith('resp:')) {
-    return;
-  }
-
-  const spl = buffer.split(':');
-  console.log(spl);
-
-  if (spl.length < 3) {
-    console.error('Received packet with no arguments.');
-  }
-
-  const handler = incomingHandlers.get(spl[1]);
-  if (!handler || spl.length <= 3) {
-    return;
-  }
-
-  handler(spl[2]);
-});
